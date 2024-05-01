58b95f9f3fea9d7c6fbf168d1ee52404c426b241
Ate329 <justin.he814@gmail.com>
2024-05-02 00:36:11 +0300

    NixOS rebuild with flakes

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -161,6 +161,12 @@
     randomizedDelaySec = "45min";
   };
 
+  nixpkgs.config.packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+      inherit pkgs;
+    };
+  };
+
   # LD fix
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
