69324fffa26742ac24eea6fff62ad47b01820996
Ate329 <justin.he814@gmail.com>
2024-05-12 01:23:39 +0300

    install hyprland

--- modules/packages.nix
+++ modules/packages.nix
@@ -10,6 +10,7 @@
      xfce.mousepad
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      wget
+     hyprland
      sl
      (wechat-uos.override { uosLicense = ./license.tar.gz; })
      okular
