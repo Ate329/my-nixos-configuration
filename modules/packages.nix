38b82bfa6792733fcf28f5bdb47fafecd81e629e
Ate329 <justin.he814@gmail.com>
2024-05-12 13:35:37 +0300

    mroe hyprlan stuff

--- modules/packages.nix
+++ modules/packages.nix
@@ -8,6 +8,15 @@
      rofi
      nh
      hyprland
+     waybar
+     eww
+     (waybar.overrideAttrs (oldAttrs: {
+     mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+       	})
+     )
+     dunst
+     mako
+     libnotify
      nitrogen
      xfce.mousepad
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
