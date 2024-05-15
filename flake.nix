99f08116d9af9232c049699a902fece559b5c99e
Ate329 <justin.he814@gmail.com>
2024-05-16 00:18:48 +0300

    Add/Remove packages

--- flake.nix
+++ flake.nix
@@ -3,6 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; 
+    hyprland.url = "github:hyprwm/Hyprland";
+
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
 
     home-manager = {
       url = "github:nix-community/home-manager";
