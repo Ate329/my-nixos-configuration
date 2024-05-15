fb2c010d9590153cddaf70c10aab72d06955e04e
Ate329 <justin.he814@gmail.com>
2024-05-15 19:51:43 +0300

    anbox

--- modules/packages.nix
+++ modules/packages.nix
@@ -6,6 +6,7 @@
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      wget
      sl
+     anbox
      hyprland
      (wechat-uos.override { uosLicense = ./license.tar.gz; })
      okular
