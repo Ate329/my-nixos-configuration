be03994b13de92ccdd9eff0214aba32264cefeaf
Ate329 <justin.he814@gmail.com>
2024-09-25 19:37:52 +0300

    update some environment variables

--- home/programs/hypr/hyprland/config.nix
+++ home/programs/hypr/hyprland/config.nix
@@ -21,16 +21,16 @@
   env = XDG_CURRENT_DESKTOP, Hyprland
   env = XDG_SESSION_TYPE, wayland
   env = XDG_SESSION_DESKTOP, Hyprland
-  env = GDK_BACKEND, wayland, x11
+  env = GDK_BACKEND, wayland,x11,*
   env = GDK_SCALE, 2
   env = XCURSOR_SIZE,24
   env = CLUTTER_BACKEND, wayland
-  env = QT_QPA_PLATFORM=wayland;xcb
+  env = QT_QPA_PLATFORM, wayland;xcb
   env = QT_WAYLAND_DISABLE_WINDOWDECORATION, 1
   env = QT_AUTO_SCREEN_SCALE_FACTOR, 1
-  env = SDL_VIDEODRIVER, x11
+  env = SDL_VIDEODRIVER, wayland,x11
   env = MOZ_ENABLE_WAYLAND, 1
-  env = AQ_NO_MODIFIERS, 1 # For systems with limitation (iGPU)
+  # env = AQ_NO_MODIFIERS, 1 # For systems with limitation (iGPU)
 
   # Monitor configuration
   monitor = eDP-1, highres, auto, auto
