8fc97423ededfe08b9edb966459507fc324ae70c
Ate329 <justin.he814@gmail.com>
2024-09-29 16:31:15 +0300

    update

--- modules/hardware/amd-drivers.nix
+++ modules/hardware/amd-drivers.nix
@@ -15,12 +15,19 @@ in
 
   config = mkIf cfg.enable {
     systemd.tmpfiles.rules = [ "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}" ];
+
     services.xserver.videoDrivers = [ "amdgpu" ];
+
     # OpenGL
     hardware.graphics = {
-      ## amdvlk: an open-source Vulkan driver from AMD
-      extraPackages = [ pkgs.amdvlk ];
-      extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
+      extraPackages = with pkgs; [
+        amdvlk
+        mesa.drivers
+      ];
+      extraPackages32 = with pkgs.driversi686Linux; [
+        amdvlk
+        mesa.drivers
+      ];
     };
   };
 }
